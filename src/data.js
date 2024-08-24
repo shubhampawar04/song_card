a503d145cbbb293a13a49fc8ddcd7bf24b4471e0
shubhampawar04 <pawarshubham81668@gmail.com>
2024-08-24 18:40:20 +0530

    added files

---
+++ src/data.js
@@ -0,0 +1,54 @@
+
+const SongAlbum = [
+  {
+      id : "shivaji Maharaj",
+      title: "Aamhi Marathe",
+      artist: "Rapperiya Baalam",
+      image : "https://i.pinimg.com/736x/aa/84/74/aa8474aae7cf32ead243df0463226383.jpg",
+      rating : 5.5
+  },
+
+  {
+      id : "Hanuman Chalisa",
+      title: "Hanuman Chalisa",
+      artist: " Gulshan Kumar || Hariharan",
+      image : "https://media.licdn.com/dms/image/D4D12AQHSMDOPb9YPhg/article-cover_image-shrink_720_1280/0/1684390609012?e=2147483647&v=beta&t=IBa97pBYsaCt7jk1A4xMjTu3vyj5XR0V4hSpTkLb2Ks",
+      rating : 5.9
+  },
+
+  {
+    id : "Bholenath",
+    title: "Mahadev",
+    artist: "Hansraj Raghuwanshi",
+    image : "https://cdn.pixabay.com/photo/2024/03/08/04/22/ai-generated-8619794_640.png",
+    rating : 5.0
+},
+
+  {
+      id : "Shree Krishna",
+      title: "Shri Krishna Govind Hare Murari",
+      artist: " Simpal Kharel",
+      image : "https://www.shutterstock.com/shutterstock/photos/2318267337/display_1500/stock-vector-vector-illustration-of-shree-krishna-for-janmashtami-2318267337.jpg",
+      rating : 5.1
+  },
+
+  
+  {
+    id : "Mahabharat",
+    title: "Mahabharat Title Song",
+    artist: "Saurabh Raj Jain",
+    image : "https://m.media-amazon.com/images/M/MV5BZGZkYTdiMzQtMWE5My00NTg2LTlhNTctOTMxNzdhYTE4NzRlXkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_FMjpg_UX1000_.jpg",
+    rating : 6.1
+},
+
+
+{
+    id : "Jay Shree Ram",
+    title: "Mere Ghar Ram Aaye Hai",
+    artist: "Jubin Nautiyal",
+    image : "https://c.saavncdn.com/805/Prabhu-Shree-Ram-Marathi-2024-20240104172017-500x500.jpg",
+    rating : 6.5
+},
+]
+
+export default SongAlbum;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SongCard from './components/SongCard/SongCard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <SongCard/>
+);
